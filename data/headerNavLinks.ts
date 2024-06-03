547af7416046ba1624dd9b4f1c3b39a0a3bdb320
Tim Damen <tim.damen80@gmail.com>
2024-06-03 14:55:55 +0200

    blog and nav link

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/speaking', title: 'Speaking' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About me' },
   { href: '/contact', title: 'Contact' },
 ]
